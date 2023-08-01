e8112d19f3b7da6dc4e44108c5cf87bbe4577095
Mateo Quotteri <quotterimateo@gmail.com>
2023-08-01 19:43:47 -0300

    adding Scanner things

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+import java.util.Scanner;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     /*
@@ -35,11 +36,36 @@ Informar el resultado obtenido.
     public static void main(String[] args) {
       //  Animal jack = new Animal("Jack", 9, "Perro");
        // System.out.println(jack.toString());
+        Integer numeroA = 1;
+        Integer numeroB = 23;
+        String nombre;
+        String apellido;
+        Integer edad;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Escribe tu nombre: ");
+        nombre = scanner.nextLine();
+        System.out.println("Ahora escribe tu edad: ");
+        edad = scanner.nextInt();
+
+        System.out.println("Tu nombre es: "+ nombre +" y tu edad es: "+ edad);
+
+        if(esDivisible(numeroA,numeroB)){
+            System.out.println("Es divisible");
+        }else {
+            System.out.println("No es divible");
+        }
+    }
+    public static Boolean esDivisible(Integer numeroA, Integer numeroB){
+        return numeroA % numeroB == 0;
+    }
+
+
 
         CryptoCurrency Ether = new CryptoCurrency(1896, "Ethereum" ,
                 "ETH" , 4695, 8);
 
-        System.out.println(Ether.getName() + " tiene este precio: " + Ether.getPrice());
+        // System.out.println(Ether.getName() + " tiene este precio: " + Ether.getPrice());
 
     /*
           Para representar cada una de las características de las mascotas, crear una variable y darle
